1b2187034b2714ddcb5b04ca1f84ef4fde7e0cde
Kenneth Baylosis <kbaylosis@zoogtech.com>
2020-05-03 14:48:54 +0800

    Fix pub-cache path issue

--- lib/src/action.dart
+++ lib/src/action.dart
@@ -1,18 +1,22 @@
 import 'package:recase/recase.dart';
 
 import 'generator.dart';
+import 'paths.dart';
 import 'template_engine.dart';
 
 class Action extends Generator {
   Action(String name, String destination) : super(name, destination);
 
   void generate(String actionName) {
+    var src = Paths.getPubCacheDir();
     var t = templates;
     t.items['Sample'] = ReCase(actionName).pascalCase;
     final filename = ReCase(actionName).constantCase.toLowerCase();
 
     print('[${t.items['Sample']}Action]');
-    TemplateEngine().convert('${srcModulePath}/actions/sample_action.template',
-        '${destModulePath}/actions/${filename}_action.dart', t);
+    TemplateEngine().convert(
+        '${src}/${srcModulePath}/actions/sample_action.template',
+        '${destModulePath}/actions/${filename}_action.dart',
+        t);
   }
 }
--- lib/src/module.dart
+++ lib/src/module.dart
@@ -3,14 +3,17 @@ import 'package:recase/recase.dart';
 
 import 'action.dart';
 import 'page.dart';
+import 'paths.dart';
 import 'template_engine.dart';
 
 class Module extends Generator {
   Module(String name, String destination) : super(name, destination);
 
   void generate() {
+    var src = Paths.getPubCacheDir();
+
     print('[Main ${ReCase(name).titleCase} file]');
-    TemplateEngine().convert('${srcModulePath}/noname.template',
+    TemplateEngine().convert('${src}/${srcModulePath}/noname.template',
         '${destModulePath}/${name}.dart', templates);
 
     Action(name, destination).generate('sample');
